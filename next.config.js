5e2ebab7a6ca1fe2fa13a1a028561f316080b9a8
Ruben Schoofs <Ruben.Schoofs@student.pxl.be>
2023-04-09 22:35:32 +0200

    Add config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   assetPrefix: isProd ? '/Portfolio-I-Talent-Ruben/' : '',
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
